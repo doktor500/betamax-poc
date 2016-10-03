9122ac488fc81916e33e5a4ea0c3a02134feb6e8
David Molinero <dmolinero@ebay.com>
2016-10-03 16:39:32 +0100

    Move URL to config

--- src/main/java/uk/co/kenfos/controller/MainController.java
+++ src/main/java/uk/co/kenfos/controller/MainController.java
@@ -1,5 +1,6 @@
 package uk.co.kenfos.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,8 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class MainController {
 
-    private static final String URL = "http://localhost:3000/healthcheck";
+    @Value("${payment.service.url}")
+    private String URL;
 
     @RequestMapping("/")
     public String index() {
